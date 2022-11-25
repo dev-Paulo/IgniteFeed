bb6020cdf381bbe0d5da49d4add518d4a6d900da
Paulo Renato <dev_paulo@outlook.com>
2022-11-25 10:51:55 -0300

    Foto de perfil alterada

--- src/App.tsx
+++ src/App.tsx
@@ -23,8 +23,8 @@ function App() {
     {
       id: 2,
       author: {
-        avatarUrl: "https://github.com/thsrossi.png",
-        name: "John Doe",
+        avatarUrl: "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+        name: "Dou Gie",
         role: "Frontend Developer "
       },
       content: [
